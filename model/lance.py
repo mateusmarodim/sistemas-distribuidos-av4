d28a3a212305cc8e016a1533ba74c4603f7341c8
Mateus V F Marodim <marodim@alunos.utfpr.edu.br>
2025-08-28 17:07:06 -0300

    Batman

---
+++ model/lance.py
@@ -0,0 +1,8 @@
+from dataclasses import dataclass
+
+@dataclass
+class Lance:
+    id_leilao: int
+    id_usuario: int
+    valor: float
+
---
+++ model/leilao.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+
+class StatusLeilao(Enum):
+    ATIVO = "ativo"
+    ENCERRADO = "encerrado"
+
+@dataclass
+class Leilao:
+    id: int
+    descricao: str
+    inicio: datetime
+    fim: datetime
+    status: StatusLeilao
