7cbb45b32310a8f79fc8baf9b97f2b65ef180a95
Mateus Vinícios Franceschetto Marodim <mateus.marodim@wises.com.br>
2025-09-09 22:50:58 -0300

    commitao 2

--- model/lance.py
+++ model/lance.py
@@ -6,3 +6,12 @@ class Lance:
     id_usuario: int
     valor: float
 
+    @classmethod
+    def from_dict(cls, data: dict):
+        """Create a Lance instance from a dictionary"""
+        return cls(
+            id_leilao=data['id_leilao'],
+            id_usuario=data['id_usuario'],
+            valor=float(data['valor'])
+        )
+
